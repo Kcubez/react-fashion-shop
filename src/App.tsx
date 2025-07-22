c97b7f1bdb1b2bcf19d341728f4896fda01e30c7
Kaung Khant Kyaw <kcubez21@gmail.com>
2025-07-22 20:55:48 +0700

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/*" element={<Error />}></Route>
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
